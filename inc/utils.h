155c5763d3668585d7aac9c9be9b4ae2a1a7b783
nicholascabrera <ncabrera528@gmail.com>
2026-06-21 11:38:14 -0400

    added utils and cmake include

---
+++ inc/utils.h
@@ -0,0 +1,11 @@
+//
+// Created by ncabr on 6/19/26.
+//
+#ifndef NECS_UTILS_H
+#define NECS_UTILS_H
+
+#include <stdint.h>
+
+typedef uint32_t Entity;
+
+#endif //NECS_UTILS_H
--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
+//
+// Created by ncabr on 6/19/26.
+//
 #include <stdio.h>
+#include "utils.h"
 
 int main(void) {
     printf("Hello, World!\n");
